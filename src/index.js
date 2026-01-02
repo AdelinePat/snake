81a5ec1ddd76f596cf7831d171dcc5e9a56fb5a0
AdelinePat <adeline.patenne@hotmail.fr>
2026-01-02 13:51:22 +0100

    feat: game functional, add apple asset instead of drawing red square

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import appleFile from "./assets/apple.png";
 
 // const HEIGHT = 500;
 const WIDTH = 800;
@@ -6,41 +7,49 @@ const NUMBEROFCOLUMN = 20;
 const NUMBEROFLINE = 12;
 const gridElem = WIDTH / NUMBEROFCOLUMN;
 const HEIGHT = gridElem * NUMBEROFLINE;
-const SPEED = 500;
+let speed = 400;
 // Grid is 20 x 12 cell (40x40px)
+const appleImg = new Image();
+appleImg.src = appleFile;
+let assetsReady = false;
+appleImg.onload = () => {
+  assetsReady = true;
+};
 
 const canvas = document.querySelector("canvas");
 canvas.setAttribute("height", HEIGHT);
 canvas.setAttribute("width", WIDTH);
 
+const keyToDirection = {
+  ArrowUp: "up",
+  KeyW: "up",
+  ArrowDown: "down",
+  KeyS: "down",
+  ArrowLeft: "left",
+  KeyA: "left",
+  ArrowRight: "right",
+  KeyD: "right",
+};
+
 const ctx = canvas.getContext("2d");
 let direction = "up";
+let score = 0;
+
 const snake = [
   [9, 9],
   [8, 9],
   [7, 9],
 ];
 
-const apple = [5, 5];
+let apple = [5, 5];
 
 function gameOver() {
   if (
-    ((direction === "right" || direction === "left") &&
-      snake[0][0] >= NUMBEROFCOLUMN) ||
-    snake[0][1] >= NUMBEROFCOLUMN ||
-    snake[0][0] < 0 ||
-    snake[0][1] < 0
-  ) {
-    console.log("perdu!");
-    return true;
-  } else if (
-    ((direction === "up" || direction === "down") &&
-      snake[0][0] >= NUMBEROFLINE) ||
+    snake[0][0] >= NUMBEROFCOLUMN ||
     snake[0][1] >= NUMBEROFLINE ||
     snake[0][0] < 0 ||
     snake[0][1] < 0
   ) {
-    console.log("perdu!");
     return true;
   } else {
     const [head, ...body] = snake;
@@ -66,8 +75,31 @@ function drawSnake() {
 }
 
 function drawApple() {
-  ctx.fillStyle = "red";
-  ctx.fillRect(apple[0] * gridElem, apple[1] * gridElem, gridElem, gridElem);
+  if (!assetsReady) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(apple[0] * gridElem, apple[1] * gridElem, gridElem, gridElem);
+  }
+
+  ctx.drawImage(
+    appleImg,
+    apple[0] * gridElem,
+    apple[1] * gridElem,
+    gridElem,
+    gridElem
+  );
+}
+
+function generateApple() {
+  const [x, y] = [
+    Math.trunc(Math.random() * NUMBEROFCOLUMN - 1),
+    Math.trunc(Math.random() * NUMBEROFLINE - 1),
+  ];
+  for (let body of snake) {
+    if (body[0] === x && body[1] === y) {
+      return generateApple();
+    }
+  }
+  apple = [x, y];
 }
 
 function updateSnakePosition() {
@@ -91,8 +123,24 @@ function updateSnakePosition() {
     }
   }
   snake.unshift(head);
-  snake.pop();
+  if (head[0] === apple[0] && head[1] === apple[1]) {
+    score += 1;
+    if (speed <= 900 && speed + score * 50 <= 900) {
+      speed += score * 50;
+    }
+    generateApple();
+  } else {
+    snake.pop();
+  }
+  //   console.log(snake[0][0], snake[0][1]);
   return gameOver();
+}
+
+function drawScore() {
+  ctx.fillStyle = "white";
+  ctx.font = "40px sans-serif";
+  ctx.textBaseline = "top";
+  ctx.fillText(score, gridElem, gridElem);
 }
 
 function move() {
@@ -100,39 +148,20 @@ function move() {
     drawMap();
     drawSnake();
     drawApple();
+    drawScore();
     setTimeout(() => {
       requestAnimationFrame(move);
-    }, 1000 - SPEED);
+    }, 1000 - speed);
   } else {
-    alert("Perdu!");
+    alert("Perdu! Votre score est de : ", score);
   }
 }
 
-// drawMap();
-// drawSnake();
-// drawApple();
 window.addEventListener("keydown", (event) => {
-  switch (event.code) {
-    case "ArrowDown":
-    case "keyS": {
-      direction = "down";
-      break;
-    }
-    case "ArrowUp":
-    case "keyZ": {
-      direction = "up";
-      break;
-    }
-    case "ArrowLeft":
-    case "keyQ": {
-      direction = "left";
-      break;
-    }
-    case "ArrowRight":
-    case "keyD": {
-      direction = "right";
-      break;
-    }
+  const newDirection = keyToDirection[event.code];
+  if (!newDirection) {
+    return;
   }
+  direction = newDirection;
 });
 requestAnimationFrame(move);
